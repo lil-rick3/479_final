f10556742a5d153b6d5e3ec4793215640c93d716
lil-rick3 <83485898+lil-rick3@users.noreply.github.com>
2023-04-23 16:32:29 -0700

    Add files via upload

---
+++ finalproj/node.py
@@ -0,0 +1,18 @@
+from graphics import*
+class Node():
+    def __init__(self, xPos, yPos, window, id):
+        self.x = xPos
+        self.y = yPos
+        self.pos = Point(self.x, self.y)
+        self.edges = []
+        self.win = window
+        self.id = id
+        self.c = Circle(self.pos, 10)
+        self.c.setFill(color_rgb(200,0,0))
+        self.name = Text(Point(self.x - 20, self.y), str(self.id))
+        self.name.setTextColor(color_rgb(250, 243, 40))
+
+
+    def draw(self):
+        self.c.draw(self.win)
+        self.name.draw(self.win)
